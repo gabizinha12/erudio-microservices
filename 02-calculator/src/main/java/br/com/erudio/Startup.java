bb970fa0648c60c4cd9d7f18a52d6d7cf09876e8
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-11-02 23:58:40 -0300

    Aula 5

--- 02-calculator/src/main/java/br/com/erudio/Startup.java
+++ 02-calculator/src/main/java/br/com/erudio/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BarApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BarApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
